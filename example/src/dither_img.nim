6e089bf22deab8341fa84076cea7858e180c04e8
Nycto <James@RoundEights.com>
2024-01-29 21:31:57 -0800

    Add palette support to example bin

--- example/src/dither_img.nim
+++ example/src/dither_img.nim
@@ -1,14 +1,25 @@
-import dither, std/os, pixie
+import dither, std/[os, algorithm, strutils], pixie
 
-type BlackAndWhite = object
+type
+    Palette = ref object
+        colors: seq[ColorRGBX]
 
-let black = rgbx(0, 0, 0, 255)
-let white = rgbx(255, 255, 255, 255)
+    DitherModes {.pure.} = enum Bayer4x4
 
-proc nearestColor(palette: BlackAndWhite, color: ColorRGBX): ColorRGBX =
-    result = if distance(color, black) > distance(color, white): white else: black
+proc nearestColor(palette: Palette, color: ColorRGBX): ColorRGBX =
+    var currentDistance: float32 = high(float32)
+    for possible in palette.colors:
+        let thisDistance = distance(possible, color)
+        if thisDistance < currentDistance:
+            result = possible
+            currentDistance = thisDistance
 
-proc approxMaxColorDistance(palette: BlackAndWhite): int = 128
+proc approxMaxColorDistance(palette: Palette): int =
+    let colors = palette.colors .sortedByIt(distance(rgbx(0, 0, 0, 255), it))
+    result = 10
+    for i, color in colors:
+        if i > 0:
+            result = max(result, distance(color, colors[i - 1]).toInt)
 
 let img = readImage(paramStr(1))
 
@@ -23,6 +34,11 @@ proc `+`(a: uint8, b: SomeInteger): uint8 = clamp(a.int + b.int, 0, 255).uint8
 proc `+`(pixel: ColorRGBX, scalar: int): ColorRGBX =
     result = rgbx(pixel.r + scalar, pixel.g + scalar, pixel.b + scalar, pixel.a)
 
-orderedDither(img, output, BlackAndWhite(), Bayer4x4)
+var palette = Palette()
+for param in 3..paramCount():
+    palette.colors.add(parseHex(paramStr(param)).rgbx)
+
+case parseEnum[DitherModes](paramStr(2))
+of DitherModes.Bayer4x4: img.orderedDither(output, palette, Bayer4x4)
 
 output.writeFile("output.png")
